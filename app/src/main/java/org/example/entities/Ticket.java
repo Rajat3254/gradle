2b4b4d51fc02dbf07d2dc4e16dcc7fe8de008e79
Rajat Singh <singhr3234@gmail.com>
2025-08-07 15:32:54 +0530

    aaj hoga

---
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -0,0 +1,12 @@
+package org.example.entities;
+
+import java.sql.Date;
+
+public class Ticket {
+    private String userId;
+    private String ticketId;
+    private String source;
+    private String destination;
+    private Date dateOfTravel;
+    private Train train;
+}
